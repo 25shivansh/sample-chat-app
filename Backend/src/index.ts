25d00e20d7870a140410d94c926a88dea2b3cce3
25shivansh <shivansh.singh4work@gmail.com>
2025-02-02 16:12:33 +0530

    BE final commit

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,17 +4,38 @@ interface User{
     socket :WebSocket;
     room: string;
 }
-let allSockets :WebSocket[]=[];
+let allSockets :User[]=[];
 wss.on("connection",(socket)=>{
-    allSockets.push(socket)
+    
     
     socket.on("message",(message)=>{
+        //@ts-ignore
+        const parsedMessage=JSON.parse(message);
+        
+        if(parsedMessage.type==="join"){
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type==="chat"){
+            let currentUserRoom=null;
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i].socket==socket){
+                    currentUserRoom=allSockets[i].room;
+                }
+            }
+            for(let i=0 ; i<allSockets.length;i++){
+                if(allSockets[i].room==currentUserRoom){
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
         
         
     })
-    socket.on("disconect",()=>{
-        allSockets=allSockets.filter(x=>x!=socket);
-    })
+
+    
 
 
 })
